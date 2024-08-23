ffaed466feded2d4bb047b3a29686c8a02d09e9b
zensei~ <bhargavburgul@gmail.com>
2024-08-23 17:37:11 +0530

    updatēd url

--- backend/app.js
+++ backend/app.js
@@ -10,21 +10,14 @@ const app = express();
 const CLIENT_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 // Middleware
-// app.use(
-// 	cors({
-// 		origin: CLIENT_URL,
-// 		credentials: true,
-// 	})
-// );
-
 app.use(
 	cors({
-	  origin: '*', // Allow all origins
-	  methods: "GET,POST,PUT,DELETE,OPTIONS",
-	  allowedHeaders: "Content-Type,Authorization",
-	  credentials: true, // Allow cookies and credentials
+		origin: CLIENT_URL,
+		credentials: true,
 	})
-  );
+);
+
+
 
 app.use(express.json());
 
